e82e4545c04f100bfa7a6456d9c9c73cadc592bb
AronSn0w <raycuasa80@gmail.com>
2024-06-07 19:32:55 -0400

    App union entre apps

--- examen/urls.py
+++ examen/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('verdevida/', include('verdevida.urls')),
+    path('', include('union.urls'))
 ]
